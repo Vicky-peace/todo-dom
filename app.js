8f677e720e13fe3799e59c0957f6175a55ec5fdd
Vicky-peace <victorpeace47@gmail.com>
2024-05-18 17:09:13 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () =>{
+    const todoInput = document.getElementById('todo-input');
+    const submitBtn = document.getElementById('submit-btn');
+    const todoList = document.getElementById('inputs');
+    const itemsLeft = document.querySelector('.items-left span');
+    
+})
